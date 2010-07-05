71fef52cf7eadf42812498b2ab07c0ed495f13af
Thav <thavilden@gmail.com>
2010-07-05 16:26:36 -0400

    Annotating with rake models:annotate

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -12,6 +12,8 @@
 #  person_id     :integer(4)
 #  staff         :boolean(1)
 #  member        :boolean(1)
+#  start_at      :datetime
+#  end_at        :datetime
 #
 
 class Visit < ActiveRecord::Base
