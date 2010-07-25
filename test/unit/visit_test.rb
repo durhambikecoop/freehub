0c1967ad5951b6ff7b2b4866d87028742e7984f2
Thav <thavilden@gmail.com>
2010-07-25 14:24:52 -0400

    Fixed some errors in testing I missed on last pass

--- test/unit/visit_test.rb
+++ test/unit/visit_test.rb
@@ -8,16 +8,16 @@ class VisitTest < ActiveSupport::TestCase
   end
 
   def test_for_organization_paged
-    assert_equal 102, Visit.for_organization(organizations(:sfbk)).paginate.size
+    assert_equal 103, Visit.for_organization(organizations(:sfbk)).paginate.size
     assert_equal 20, Visit.for_organization(organizations(:sfbk)).paginate.to_a.size
-    assert_equal 102, Visit.for_organization(organizations(:sfbk)).paginate(:current => 2, :size => 10).size
+    assert_equal 103, Visit.for_organization(organizations(:sfbk)).paginate(:current => 2, :size => 10).size
     assert_equal 10, Visit.for_organization(organizations(:sfbk)).paginate(:current => 2, :size => 10).to_a.size
   end
 
   def test_for_organization_in_date_range
     from, to = Date.new(2007,2,1), Date.new(2007,2,3)
     assert_equal 99, Visit.for_organization(organizations(:sfbk)).before(from).size
-    assert_equal 3, Visit.for_organization(organizations(:sfbk)).after(from).before(to).size
+    assert_equal 4, Visit.for_organization(organizations(:sfbk)).after(from).before(to).size
   end
 
   def test_paginated_association
@@ -30,7 +30,7 @@ class VisitTest < ActiveSupport::TestCase
                      :after => Date.new(2007,2,1),
                      :before => Date.new(2007,2,3) }
 
-    assert_equal 3, Visit.chain_finders(finder_chain).paginate.size
+    assert_equal 4, Visit.chain_finders(finder_chain).paginate.size
   end
 
   def test_to_csv
