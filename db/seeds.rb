7c93d4d4538cccf0e106de89d2b9b8758265cc58
jplien <jolien@vt.edu>
2012-03-31 23:03:12 -0400

    Update seeds.rb to generate all tags.  Added tag for trained greeters.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+#
+
+# Populate the DBC Tags
+Tag.create(:id => 1, :name => 'board member')
+Tag.create(:id => 2, :name => 'core volunteer')
+Tag.create(:id => 3, :name => 'key holder')
+Tag.create(:id => 5, :name => 'experienced mechanic')
+Tag.create(:id => 10, :name => 'clean energy durham clinic participant')
+Tag.create(:id => 14, :name => 'banned')
+Tag.create(:id => 16, :name => 'youth program participant')
+Tag.create(:id => 24, :name => 'deaf')
+Tag.create(:id => 25, :name => 'trained greeter')
